7cc137bc23f9d1f97a513835af9c4a8dcd8ec312
Ricardo031 <rjtb31@gmail.com>
2024-10-10 14:01:16 -0500

    Finalizacion se puede mejorar mas para que sea responsive y mejorar los estilos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+const verCarrito = document.getElementById('verCarrito');
+const modalContainer = document.getElementById('modal-container');
 
 let carrito = [];
-let comprar = document.querySelectorAll('.product__btn')
+let comprar = document.querySelectorAll('.product__btn');
 
 comprar.forEach((boton) => {
     boton.addEventListener('click', () => {
-        const idProducto = parseInt(boton.getAttribute('data-id'))
-
+        const idProducto = parseInt(boton.getAttribute('data-id'));
         const product = productos.find((p) => p.id === idProducto);
 
         if (product) {
@@ -13,9 +14,49 @@ comprar.forEach((boton) => {
             console.log(carrito);
         } else {
             console.log('Producto no encontrado');
-
         }
-    })
+    });
+});
 
+verCarrito.addEventListener('click', () => {
+    // Limpiar el contenido del modal antes de mostrar el nuevo contenido
+    modalContainer.innerHTML = '';
 
-})
+    const modalHeader = document.createElement('div');
+    modalHeader.className = 'modal-header';
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalButton = document.createElement('h1');
+    modalButton.innerText = 'X';
+    modalButton.className = 'modal-header-button';
+    //* boton X para quitar el modal
+    modalButton.addEventListener('click', () => {
+        modalContainer.classList.remove('active'); // Oculta la barra lateral
+    });
+    modalHeader.append(modalButton);
+
+    //* Agregar cada producto del carrito al modal
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement('div');
+        carritoContent.className = 'modal-content';
+        carritoContent.innerHTML = `
+            <img src="${product.img}" alt="${product.nombre}" class="modal-img" />
+            <p>${product.nombre}</p>
+            <p>${product.precio}$</p>
+        `;
+        modalContainer.append(carritoContent);
+    });
+
+    //* Mostrar la barra lateral
+    modalContainer.classList.add('active');//se activa la transicion con el modal para que se muestre al 100%
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuyind = document.createElement('div');
+    totalBuyind.className = 'total-content';
+    totalBuyind.innerHTML = `Total a pagar: ${total} $`;
+    modalContainer.append(totalBuyind)
+});
--- js/productos.js
+++ js/productos.js
@@ -3,25 +3,30 @@ const productos = [
         id: 1,
         nombre: "Banana",
         precio: 50,
+        img:"img/banana.jpg",
     },
     {
         id: 2,
         nombre: "Zanahoria",
         precio: 100,
+        img: "img/zanahoria.jpg",
     },
     {
         id: 3,
         nombre: "Avena",
         precio: 150,
+        img: "img/avena.jpg",
     },
     {
         id: 4,
         nombre: "Gaseosa",
-        precio: 200
+        precio: 200,
+        img: "img/gaseosa.jpg",
     },
     {
         id: 5,
         nombre: "Cerveza",
         precio: 250,
+        img: "img/cerveza.jpg",
     },
 ]
